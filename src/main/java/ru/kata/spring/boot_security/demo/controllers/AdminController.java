56d1b4904bc5bdd43dac69b9b37f1f1a8c3c994d
renJava <rendg68@gmail.com>
2023-07-07 20:45:08 +0300

    1. Неправильно работает логика User 2. Сделать жёлтыми заполненные поля Add new user

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,16 +1,20 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entities.Role;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,20 +27,28 @@ public class AdminController {
         this.userService = userService;
     }
 
+    static final String rolesS = "roles";
+
     @Autowired
     public void setRoleService(RoleService roleService) {
         this.roleService = roleService;
     }
 
     @GetMapping
-    public String printUsers(Model model) {
+    public String printUsers(Model model, @ModelAttribute("newUser") User newUser, Principal principal) {
+        User authenticatedUser = userService.findByUsername(principal.getName());
+        model.addAttribute("authenticatedUser", authenticatedUser);
+        model.addAttribute("authenticatedUserRoles", authenticatedUser.getRoles());
         model.addAttribute("users", userService.findAll());
+        model.addAttribute(rolesS, roleService.getAllRoles());
         return "user-admin";
     }
 
     @GetMapping("/user-create")
-    public String createUserForm(Model model) {
-        model.addAttribute("roles", roleService.getAllRoles());
+    public String createUserForm(Model model, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("currentUser", userService.findByUsername(userDetails.getUsername()));
+        model.addAttribute(rolesS, roleService.getAllRoles());
         model.addAttribute("user", new User());
         return "user-create";
     }
@@ -44,10 +56,13 @@ public class AdminController {
     static final String REDIRECT_ADMIN = "redirect:/admin";
 
     @PostMapping
-    public String addUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+    public String addUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
+                          @ModelAttribute("currentUser") User currentUser,
+                          @ModelAttribute("roles") List<Role> roles) {
         if (bindingResult.hasErrors()) {
             return "user-create";
         }
+
         userService.saveUser(user);
         return REDIRECT_ADMIN;
     }
@@ -58,17 +73,10 @@ public class AdminController {
         return REDIRECT_ADMIN;
     }
 
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("roles", roleService.getAllRoles());
-        model.addAttribute("user", userService.findById(id));
-        return "user-update";
-    }
-
-    @PatchMapping("/user-update")
+    @PatchMapping("/user-create/{id}") //TODO user-create вместо user-update
     public String updateUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "user-update";
+            return REDIRECT_ADMIN;
         }
 
         userService.saveUser(user);
