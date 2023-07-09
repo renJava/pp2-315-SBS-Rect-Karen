181ae29c177d53cc93a3ceae67a492cd6ad4e022
renJava <rendg68@gmail.com>
2023-07-09 12:48:39 +0300

    Поправил файл light-yellow-field-314.js - сделал короче

--- src/main/resources/static/js/light-yellow-field-314.js
+++ src/main/resources/static/js/light-yellow-field-314.js
@@ -1,30 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var nameInput = document.getElementById("name");
-    var lastNameInput = document.getElementById("lastName");
-    var usernameInput = document.getElementById("username");
+    let inputs = [
+        document.getElementById("name"),
+        document.getElementById("lastName"),
+        document.getElementById("username")
+    ];
 
-    nameInput.addEventListener("input", function () {
-        if (nameInput.value !== "") {
-            nameInput.style.backgroundColor = "rgb(254,232,149)";
-        } else {
-            nameInput.style.backgroundColor = "";
-        }
+    inputs.forEach(function (input) {
+        input.addEventListener("input", function () {
+            if (input.value !== "") {
+                input.style.backgroundColor = "rgb(254,232,149)";
+            } else {
+                input.style.backgroundColor = "";
+            }
+        });
     });
-
-    lastNameInput.addEventListener("input", function () {
-        if (lastNameInput.value !== "") {
-            lastNameInput.style.backgroundColor = "rgb(254,232,149)";
-        } else {
-            lastNameInput.style.backgroundColor = "";
-        }
-    });
-
-    usernameInput.addEventListener("input", function () {
-        if (usernameInput.value !== "") {
-            usernameInput.style.backgroundColor = "rgb(254,232,149)";
-        } else {
-            usernameInput.style.backgroundColor = "";
-        }
-    });
-
 });
